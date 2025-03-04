706af86c029171c022ad03c48c97a99213ad383e
diego <diegosof28512@gmail.com>
2025-03-03 22:45:40 -0600

    test_wordtopdf comentarios

--- test_wordtopdf/main.py
+++ test_wordtopdf/main.py
@@ -4,13 +4,12 @@ from docx2pdf import convert
 
 # dropbox_ruta_relativa = 'Dropbox/test_wordtopdf'
 directorio_actual = os.path.dirname(os.path.abspath(__file__))
+
 # Convertimos directamente
 # convert("test.docx")
-# Podemos definir un archivo PDF de salida
-# convert("test", "test.pdf")
+
 # O también convertir todo el contenido de una carpeta
 # convert("/Dropbox/test_wordtopdf")
-# ruta_archivo_dropbox = os.path.join(directorio_actual, dropbox_ruta_relativa)
 
 # print(directorio_actual)
 
